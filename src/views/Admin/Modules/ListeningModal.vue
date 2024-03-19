63d07f43f070b3f4f8846abdf9437769f32408e3
cnint627 <cnint627@hust.edu.cn>
2024-03-19 10:40:41 +0800

    3.16 听力双题型

--- src/views/Admin/Modules/ListeningModal.vue
+++ src/views/Admin/Modules/ListeningModal.vue
@@ -66,6 +66,61 @@
           </a-col>
         </a-row>
         <a-button :disabled="isEditQuestion" :style="isEditQuestion?'background-color: gray':''" id="q-button" @click="handleEditQuestion">编辑题目</a-button>
+        <!-- 填空题部分结束 -->
+          <a-card style="width: 100%;margin:20px 0" v-for="(question, q_index) in model.questions" v-bind:key="question">
+            <a-form-model-item
+                label="题目标题"
+                :prop="'questions.'+q_index+'.title'"
+                :rules="[
+                  {required: true, message: '请输入题目标题！'},
+                  {min: 0, max: 300, message: '长度不超过 300 个字符', trigger: 'blur'},
+                ]"
+                :label-col="labelCol"
+                :wrapper-col="wrapperCol"
+            >
+              <a-input
+                  v-model="model.questions[q_index].title"
+                  placeholder="请输入题目标题" />
+            </a-form-model-item>
+            <a-form-model-item
+                label="题目答案"
+                :prop="'questions.'+q_index+'.answer'"
+                :rules="[
+                  {required: true, message: '请输入题目答案！'},
+                  {min: 0, max: 100, message: '长度不超过 100 个字符', trigger: 'blur'},
+                ]"
+                :label-col="labelCol"
+                :wrapper-col="wrapperCol"
+            >
+              <a-input
+                  v-model="model.questions[q_index].answer"
+                  placeholder="请输入题目答案" />
+            </a-form-model-item>
+            <a-row style="width: 100%" v-for="(option, o_index) in model.questions[q_index].options" v-bind:key="option">
+              <a-col :span="22">
+                <a-form-model-item
+                    label="题目选项"
+                    :prop="'questions.'+q_index+'.options.'+o_index+'.content'"
+                    :rules="[
+                    {required: true, message: '请输入题目选项！'},
+                    {min: 0, max: 300, message: '长度不超过 300 个字符', trigger: 'blur'},
+                  ]"
+                    :label-col="labelCol"
+                    :wrapper-col="wrapperCol"
+                >
+                  <a-input
+                      v-model="model.questions[q_index].options[o_index].content"
+                      placeholder="请输入题目选项" />
+                </a-form-model-item>
+              </a-col>
+              <a-col :span="2">
+                <a-button style="margin-left: 10px;background-color: red;color:white;margin-top: 3px" @click="handleDeleteOption(q_index,o_index)">删除</a-button>
+              </a-col>
+            </a-row>
+            <a-button id="o-button" @click="handleAddOption(q_index)">新增选项</a-button>
+            <a-button id="o-button" style="background-color: red" @click="handleDeleteQuestion(q_index)">删除题目</a-button>
+          </a-card>
+          <a-button id="q-button" @click="handleAddQuestion">新增题目</a-button>
         <div id="action">
           <a-button id="action-button" style="background-color: lightgreen" @click="handleOk">确定</a-button>
           <a-button id="action-button" @click="handleCancel">取消</a-button>
@@ -109,6 +164,7 @@ export default {
         title: '',
         audioPath:'',
         blanks: [],
+        questions:[],
         // 听力材料内容，后续会对其进行转化处理
         content:''
       },
@@ -136,14 +192,23 @@ export default {
             message.error("请上传音频文件", 1)
             return
           }
-          if (this.blankList.length === 0) {
-            message.error("至少选取一个单词作为题目", 1)
-            return
+          // if (this.blankList.length === 0) {
+          //   message.error("至少选取一个单词作为题目", 1)
+          //   return
+          // }
+          for (let index in this.model.questions) {
+            if (this.model.questions[index].options.length === 0) {
+              message.error("选项不能为空", 1)
+              return
+            }
           }
           this.confirmLoading = true
 
           // 将挖空转化为模型
           this.parseBlankToModel();
+          console.log(this.model)
+          // 设置题目编号
+          this.setSortNum();
           console.log(this.model)
 
           // 上传音频文件
@@ -190,16 +255,16 @@ export default {
 
     // 按下编辑题目
     handleEditQuestion(){
-      this.$refs.form.validate(val => {
-        if(val){
-          this.isEditQuestion=true
-          this.model.content=this.model.content.replace(/\n/g,"\n ");
-          this.model.content=this.model.content.split(" ")
-          console.log(this.model.content)
-        }else{
-          return false
-        }
-      })
+        this.isEditQuestion=true
+        this.model.content=this.model.content.replace(/\n/g,"\n ");
+        this.model.content=this.model.content.split(" ")
+        // for(let i in this.model.content){
+        //   this.model.content[i]=this.model.content[i].replace(/^\s*((?:[\S\s]*\S)?)\s*$/, '$1')
+        //   if(this.model.content[i]===''){
+        //     this.model.content.splice(i,1)
+        //   }
+        // }
+        console.log(this.model.content)
     },
 
     // 选择要挖空的单词
@@ -218,21 +283,27 @@ export default {
     // 对挖好空的模型进行转化
     parseBlankToModel(){
       this.model.blanks=[]
-      this.blankList.sort()
+      // 这里是sort的一个坑，不传排序函数时会先将数字转换为字符串再根据ASCII排序，所以要给定排序函数
+      this.blankList.sort(function (a, b) {
+        return a - b;
+      })
+      console.log("this is sort",this.blankList)
       let blankList=this.blankList
       let content=this.model.content
       for(let index in blankList){
         this.model.blanks.push(
             {
               content:index===0?content.slice(0,blankList[index]).join(' '):content.slice(blankList[index-1]+1,blankList[index]).join(' '),
-              answer:content[blankList[index]]
+              answer:content[blankList[index]],
+              sortNum:index
             }
         )
         if(index==blankList.length-1 && blankList[index]!=content.length-1){
           this.model.blanks.push(
               {
                 content:content.slice(blankList[index]+1,content.length).join(' '),
-                answer:null
+                answer:null,
+                sortNum:-1
               }
           )
         }
@@ -285,6 +356,53 @@ export default {
     // 生成随机ID
     randomID() {
       return Number(Math.random().toString().substr(2,0) + Date.now()).toString(36);
+    },
+
+    // 新增题目
+    handleAddQuestion(){
+      this.model.questions.push(JSON.parse(JSON.stringify(
+          {
+            title:'',
+            options:[],
+            answer:'',
+          }
+      )))
+      console.log(this.model)
+    },
+
+    // 新增选项
+    handleAddOption(index){
+      this.model.questions[index].options.push(
+          {
+            content:''
+          }
+      )
+    },
+
+    // 删除题目
+    handleDeleteQuestion(index){
+      this.model.questions.splice(index,1)
+    },
+
+    // 删除选项
+    handleDeleteOption(q_index,o_index){
+      this.model.questions[q_index].options.splice(o_index,1)
+    },
+
+    // 为题目设置序号
+    setSortNum(){
+      // 从填空题之后开始编号
+      let cur=this.model.blanks.length
+      if(cur>0) {
+        if (!this.model.blanks[cur - 1].answer) {
+          // 如果填空最后一个不是题目，编号减一
+          cur -= 1
+        }
+      }
+      for(let index in this.model.questions){
+        this.model.questions[index].sortNum=cur
+        cur+=1
+      }
     }
   }
 };
--- src/views/Listening/ListeningDetail.vue
+++ src/views/Listening/ListeningDetail.vue
@@ -10,23 +10,47 @@
           {{word}}
           <br v-if="word && word.charAt(word.length-1)==='\n'"/>
         </span>
-        <a-input v-if="record.blanks[index].hasBlank && !isSubmited" style="width: 180px" v-model="myAnswerRecord[index]" :placeholder="index+1"></a-input>
+        <a-input v-if="record.blanks[index].hasBlank && !isSubmited" style="width: 180px" v-model="myBlankRecord[index]" :placeholder="index+1"></a-input>
         <br v-if="blank.answer && !isSubmited"/>
         <span v-if="isSubmited">
-          <span v-if="record.blanks[index].hasBlank && submitAnswerRecord[index].isCorrect" style="width: 180px;color:lightgreen">
-            {{myAnswerRecord[index]}}
-            <br v-if="submitAnswerRecord[index].answer && submitAnswerRecord[index].answer.charAt(submitAnswerRecord[index].answer.length-1)==='\n'"/>
+          <span v-if="record.blanks[index].hasBlank && submitAnswerRecord[record.blanks[index].sortNum].isCorrect" style="width: 180px;color:lightgreen">
+            {{myBlankRecord[index]}}
+            <br v-if="submitAnswerRecord[record.blanks[index].sortNum].answer && submitAnswerRecord[record.blanks[index].sortNum].answer.charAt(submitAnswerRecord[record.blanks[index].sortNum].answer.length-1)==='\n'"/>
           </span>
-          <s v-if="record.blanks[index].hasBlank && !submitAnswerRecord[index].isCorrect" style="width: 180px;color:red">
-            {{myAnswerRecord[index]}}
+          <s v-if="record.blanks[index].hasBlank && !submitAnswerRecord[record.blanks[index].sortNum].isCorrect" style="width: 180px;color:red">
+            {{myBlankRecord[index]}}
           </s>
-          <span v-if="record.blanks[index].hasBlank && !submitAnswerRecord[index].isCorrect" style="width: 180px;color:lightskyblue">
-            {{submitAnswerRecord[index].answer}}
-            <br v-if="submitAnswerRecord[index].answer && submitAnswerRecord[index].answer.charAt(submitAnswerRecord[index].answer.length-1)==='\n'"/>
+          <span v-if="record.blanks[index].hasBlank && !submitAnswerRecord[record.blanks[index].sortNum].isCorrect" style="width: 180px;color:lightskyblue">
+            {{submitAnswerRecord[record.blanks[index].sortNum].answer}}
+            <br v-if="submitAnswerRecord[record.blanks[index].sortNum].answer && submitAnswerRecord[record.blanks[index].sortNum].answer.charAt(submitAnswerRecord[record.blanks[index].sortNum].answer.length-1)==='\n'"/>
           </span>
         </span>
       </span>
     </p>
+      <!-- 选择题部分开始 -->
+      <div class="question" v-for="(question,q_index) in record.questions" v-bind:key="question">
+        <div style="margin-bottom: 20px">
+          问题 {{record.questions[q_index].sortNum+1}} : {{question.title}}
+        </div>
+        <a-radio-group v-model="myQuestionRecord[q_index]" v-if="!isSubmited">
+          <div class="option" v-for="(option,o_index) in question.options" v-bind:key="option">
+            <a-radio :value="o_index"/>选项 {{o_index+1}} : {{option.content}}
+          </div>
+        </a-radio-group>
+        <a-radio-group v-model="myQuestionRecord[q_index]" v-if="isSubmited" readonly>
+          <div
+              class="option"
+              v-for="(option,o_index) in question.options"
+              v-bind:key="option"
+              :style="(o_index==myQuestionRecord[q_index]?(submitAnswerRecord[q_index].isCorrect?'color:yellowgreen;':'color:red;'):'')+
+                    (o_index==submitAnswerRecord[q_index].answer?'color:yellowgreen;':'')"
+          >
+            <a-radio :value="o_index" onClick='javascript:return false'/>
+            选项 {{o_index+1}} : {{option.content}}
+          </div>
+        </a-radio-group>
+      </div>
+      <!-- 选择题部分结束 -->
     </div>
     <div class="listening-question-bottom">
     <a-button type="primary" style="height:50px" class="button" @click="handleSubmitAnswer" :disabled="isSubmited">提交答案</a-button>
@@ -37,21 +61,20 @@
 <script>
 import {getAction, postAction} from "@/api/action";
 import {message} from "ant-design-vue";
-
 export default {
   name: "ListeningDetail",
   data() {
     return {
       // 文章记录
       record:{},
-      // 自己的答案记录
-      myAnswerRecord:[],
+      // 自己的填空记录
+      myBlankRecord:[],
+      // 自己的选择题记录,
+      myQuestionRecord:[],
       // 提交后的答案记录
       submitAnswerRecord:[],
       // 是否提交
       isSubmited:false,
-      // 是否正在播放音频
-      isAudioPlaying:false,
       url:{
         getById:"/listening/getById",
         submitAnswer:"/listening/submitAnswer"
@@ -65,17 +88,25 @@ export default {
           console.log(res)
           if(res.data) {
             this.record = res.data
-            this.myAnswerRecord = new Array(this.record.blanks.length).fill('')
-            if(!this.record.blanks[this.record.blanks.length-1].hasBlank){
+            this.myBlankRecord = new Array(this.record.blanks.length).fill('')
+            this.myQuestionRecord=new Array(this.record.questions.length).fill(-1)
+            if(this.record.blanks.length>0 && !this.record.blanks[this.record.blanks.length-1].hasBlank){
               // 如果最后一个段落没有填空，则从我的回答记录中删除，这样页面渲染时就是正常的
-              this.myAnswerRecord.splice(0,1)
+              this.myBlankRecord.splice(0,1)
             }
             this.submitAnswerRecord=res.data.records
             if(this.submitAnswerRecord.length>0){
               this.isSubmited=true
+              let i = 0
               for(let index in this.submitAnswerRecord){
-                this.myAnswerRecord[index]=this.submitAnswerRecord[index].record
+                if(index<this.myBlankRecord.length) {
+                  this.myBlankRecord[index] = this.submitAnswerRecord[index].record
+                }else{
+                  this.myQuestionRecord[i]=parseInt(this.submitAnswerRecord[index].record)
+                  i+=1
+                }
               }
+              console.log(this.myQuestionRecord)
             }
           }else{
             // 听力不存在
@@ -88,9 +119,11 @@ export default {
   methods:{
     // 提交答案
     handleSubmitAnswer(){
-      console.log(this.myAnswerRecord)
-      if(this.validateAnswer(this.myAnswerRecord)) {
-        postAction(this.url.submitAnswer + "?id=" + this.record.id, this.myAnswerRecord)
+      console.log(this.myBlankRecord)
+      console.log(this.myQuestionRecord)
+      let myAnswerRecord=this.myBlankRecord.concat(this.myQuestionRecord)
+      if(this.validateAnswer(myAnswerRecord)) {
+        postAction(this.url.submitAnswer + "?id=" + this.record.id, myAnswerRecord)
             .then(res => {
               message.success("提交成功",1)
               console.log(res)
@@ -105,7 +138,7 @@ export default {
 
     // 检查答案合法性
     validateAnswer(myAnswerRecord){
-      return myAnswerRecord.indexOf('')===-1
+      return myAnswerRecord.indexOf('')===-1 && myAnswerRecord.indexOf(-1)===-1
     },
 
   }
@@ -156,6 +189,26 @@ export default {
   bottom:0;
   width: calc(65vw - 50px);
   border-radius: 5px;
+}
+.question{
+  display:flex;
+  flex-direction: column;
+  color:blue;
+  background-color: lightskyblue;
+  margin:30px 0;
+  border-radius: 20px;
+  padding: 30px 30px;
+  font-size: 20px;
+}
+
+.option{
+  margin:10px 0;
+  display: inline-block;
+  color:black;
+  align-items: start;
+  text-align: start;
+  width: 100%;
+  line-height: 36px;
 }
 
 
